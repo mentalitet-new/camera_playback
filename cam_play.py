729d84d7b5032edc0a1e4ec3e8828725afd12028
mentalitet-new <rybcovrg@gmail.om>
2022-04-01 12:44:28 +0300

    add cam

--- cam_play.py
+++ cam_play.py
@@ -2,8 +2,6 @@ import sys
 import cv2 as cv
 from PyQt5.QtWidgets import *
 
-
-
 class Play_Cam(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -14,25 +12,48 @@ class Play_Cam(QMainWindow):
         self.all_lable()
         self.all_input()
 
+        self.btn_show_cam = QPushButton("Choice model cam", self)
+        self.btn_show_cam.move(0, 0)
+        self.btn_show_cam.clicked.connect(self.btn_choice_model_cam)
+
+    def btn_choice_model_cam(self):
+        list_the_cam = ["Sunell", "UNV", "Dunacolor"]
+        camera, btn_clk_OK = QInputDialog.getItem(self, "Cameras_title", "Enter cam", list_the_cam)
+        if camera == "Sunell" and btn_clk_OK == True:
+            self.lable_enter_cam.setText("Sunell")
+            self.play_cam_sta_sunell()
+        elif camera == "UNV" and btn_clk_OK ==True:
+            self.lable_enter_cam.setText("UNV")
+            self.play_cam_sta_unv()
+        elif camera == "Dunacolor" and btn_clk_OK == True:
+            self.lable_enter_cam.setText("Dunacolor")
+            self.play_cam_sta_dunacolor()
+
     def all_input(self):
 
         self.input_ip = QLineEdit("", self)
         self.input_ip.move(0, 80)
         self.input_ip.resize(140, 20)
+        self.input_ip.setText("192.168.")
 
         self.input_login = QLineEdit("", self)
         self.input_login.move(0, 120)
         self.input_login.resize(100, 20)
+        self.input_login.setText("Admin")
 
         self.input_pass = QLineEdit("", self)
         self.input_pass.move(0, 160)
         self.input_pass.resize(100, 20)
+        self.input_pass.setText("1234")
 
         self.input_stream_number = QLineEdit("", self)
         self.input_stream_number.move(0, 200)
         self.input_stream_number.resize(100, 20)
 
     def all_lable(self):
+
+        self.lable_enter_cam = QLabel("your enter cam", self)
+        self.lable_enter_cam.move(320, 0)
 
         self.lable_ip = QLabel("enter IP cam", self)
         self.lable_ip.move(0, 55)
@@ -48,10 +69,6 @@ class Play_Cam(QMainWindow):
 
     def all_btn_in_window(self):
 
-        self.button_play_cum = QPushButton("Play", self)
-        self.button_play_cum.move(0, 0)
-        self.button_play_cum.clicked.connect(self.play_cam_sta)
-
         self.button_play_cum_exit = QPushButton("Close Cam", self)
         self.button_play_cum_exit.move(100, 0)
         self.button_play_cum_exit.clicked.connect(self.new_close)
@@ -63,8 +80,25 @@ class Play_Cam(QMainWindow):
         elif res == QMessageBox.No:
             QMessageBox.information(self, " info", " Continue")
 
-    def play_cam_sta(self):
+    def play_cam_sta_unv(self):
+        self.ret_key_q = 0
+        cap = cv.VideoCapture(f"rtsp://{self.input_login.text()}:{self.input_pass.text()}@{self.input_ip.text()}"
+                              f"/media/video{self.input_stream_number.text()}/video")
+        while cap.isOpened():
+            ret, frame = cap.read()
+            # if frame is read correctly ret is True
+            if not ret:
+                QMessageBox.information(self, "info", "incorrect param")
+                break
+            rgb_color = cv.cvtColor(frame, cv.COLOR_RGBA2RGB)
+            cv.imshow('frame', rgb_color)
+            if cv.waitKey(1) == ord("q") or self.ret_key_q == 113:
+                break
 
+        cap.release()
+        cv.destroyAllWindows()
+
+    def play_cam_sta_sunell(self):
         self.ret_key_q = 0
         cap = cv.VideoCapture(f"rtsp://{self.input_login.text()}:{self.input_pass.text()}@{self.input_ip.text()}"
                               f"/snl/live/1/{self.input_stream_number.text()}/cx/sido=-A0my1A==")
@@ -72,13 +106,30 @@ class Play_Cam(QMainWindow):
             ret, frame = cap.read()
             # if frame is read correctly ret is True
             if not ret:
-                QMessageBox.information(self, "info", " incorrect param")
+                QMessageBox.information(self, "info", "incorrect param")
                 break
             rgb_color = cv.cvtColor(frame, cv.COLOR_RGBA2RGB)
             cv.imshow('frame', rgb_color)
             if cv.waitKey(1) == ord("q") or self.ret_key_q == 113:
                 break
 
+        cap.release()
+        cv.destroyAllWindows()
+
+    def play_cam_sta_dunacolor(self):
+        self.ret_key_q = 0
+        cap = cv.VideoCapture(f"rtsp://{self.input_login.text()}:{self.input_pass.text()}@{self.input_ip.text()}"
+                              f":554/h264")
+        while cap.isOpened():
+            ret, frame = cap.read()
+            # if frame is read correctly ret is True
+            if not ret:
+                QMessageBox.information(self, "info", "incorrect param")
+                break
+            rgb_color = cv.cvtColor(frame, cv.COLOR_RGBA2RGB)
+            cv.imshow('frame', rgb_color)
+            if cv.waitKey(1) == ord("q") or self.ret_key_q == 113:
+                break
 
         cap.release()
         cv.destroyAllWindows()
